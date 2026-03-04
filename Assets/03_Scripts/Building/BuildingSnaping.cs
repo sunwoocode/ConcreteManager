1b06b9a794072ef29d66982cb4537507132f4c9d
sunwoocode <129387872+sunwoocode@users.noreply.github.com>
2026-03-04 19:55:17 +0900

    0304 수 저녁
    GridManager

--- Assets/03_Scripts/Building/BuildingSnaping.cs
+++ Assets/03_Scripts/Building/BuildingSnaping.cs
@@ -3,30 +3,32 @@ using UnityEngine;
 
 public class BuildingSnaping : MonoBehaviour
 {
-    private Vector3 position;
-    private float width;
-    private float length;
-    private float high;
+    private Vector3 pos;            // 건물 위치 지역 변수
+    private float width;            // 건물 가로 지역 변수
+    private float length;           // 건물 세로 지역 변수
+    private float high;             // 건물 높이 지역 변수
 
     void Start()
     {
-        width = this.transform.localScale.x;
-        length = this.transform.localScale.z;
-        high = this.transform.localScale.y;
+        width = this.transform.localScale.x;        // 건물 가로 크기
+        length = this.transform.localScale.z;       // 건물 세로 크기
+        high = this.transform.localScale.y;         // 건물 높이
     }
 
     void Update()
     {
-        position = this.transform.position;
-        this.transform.position = GetSnappedPosition(position, width, length);
+        pos = this.transform.position;     // 현재 건물 위치로 초기화
+        this.transform.position = GetSnappedPosition(pos, width, length);      // 건물 위치 0.5 단위로 정렬
+        pos = this.transform.position;
         BuildingPosition();
     }
 
     public static Vector3 GetSnappedPosition(Vector3 worldPos, float width, float length)
     {
-        bool oddX = width % 2 <= 1;    // 홀수
-        bool oddZ = length % 2 <= 1;
+        bool oddX = width % 2 <= 1;     // 건물 가로 크기가 홀수
+        bool oddZ = length % 2 <= 1;    // 건물 세로 크기가 홀수
 
+        // 
         float snapX = oddX ? Mathf.Floor(worldPos.x) + 0.5f : Mathf.Round(worldPos.x);
         float snapZ = oddZ ? Mathf.Floor(worldPos.z) + 0.5f : Mathf.Round(worldPos.z);
 
@@ -37,14 +39,16 @@ public class BuildingSnaping : MonoBehaviour
     {
         List<Vector3> bp = new List<Vector3>();
 
-        float halfWidth = width / 2;
-        float halfLength = length / 2;
-        
-        bp.Add(position);
-        bp.Add(new Vector3(position.x + halfWidth, position.y, position.z + halfLength));
-        bp.Add(new Vector3(position.x + halfWidth, position.y, position.z - halfLength));
-        bp.Add(new Vector3(position.x - halfWidth, position.y, position.z - halfLength));
-        bp.Add(new Vector3(position.x - halfWidth, position.y, position.z + halfLength));
+        float halfWidth = width / 2f;
+        float halfLength = length / 2f;
+
+        Vector3 applyPosX = new Vector3(pos.x + halfWidth, pos.y, pos.z + halfLength);
+        Vector3 applyPosY = new Vector3(pos.x - halfWidth, pos.y, pos.z - halfLength);
+
+
+        bp.Add(pos);
+        bp.Add(applyPosY);
+        bp.Add(applyPosY);
 
         BuildData data = new BuildData(this.name, high, bp);
         GridManager.instance.bpData.Add(data);
--- Assets/03_Scripts/Pathfinding/GridManager.cs
+++ Assets/03_Scripts/Pathfinding/GridManager.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using static TreeEditor.TreeEditorHelper;
@@ -19,7 +20,7 @@ public class GridManager : MonoBehaviour
     private Node[,] grid;               // 전체 격자 노드 배열
     public List<BuildData> bpData;      // 건물 데이터 리스트
 
-    private bool isLoged;
+    private bool isLoged;               // CSV 문서 초기화
 
     void Awake()
     {
@@ -36,24 +37,17 @@ public class GridManager : MonoBehaviour
 
     void Start()    // 다른 메니저에서 건물 등의 정보 불러올 때 사용
     {
-        
     }
 
     void Update()
     {
-        NDResearch();           // 노드 walkable 값 탐색
         NDInselt();             // 노드 수정
-
-        isLoged = false;
         BuildExportToCSV();     // 건물 데이터 csv 백업
+        NordExportToCSV();
+        bpData.Clear();
     }
 
-    public void NDResearch()
-    {
-
-    }
-
-    void NDInselt()
+    void NDInselt()             // walkable 데이터 수정
     {
         // grid[0, 1].walkable = true;
     }
@@ -67,7 +61,7 @@ public class GridManager : MonoBehaviour
                 grid[x, y] = new Node(new Vector2Int(x, y), true);
     }
 
-    public void NordExportToCSV()       // 노드 데이터
+    public void NordExportToCSV()       // 노드 데이터 백업
     {
         StringBuilder sb = new StringBuilder();
         sb.AppendLine("x,y,direction,walkable");
@@ -119,28 +113,47 @@ public class GridManager : MonoBehaviour
         File.WriteAllText(Application.dataPath + "/03_Scripts/Pathfinding/NodeData.csv", sb.ToString());
     }
 
-    public void BuildExportToCSV()       // 건물 데이터
+    public void BuildExportToCSV()       // 건물 데이터 백업
     {
-        if (isLoged) return;
-
         StringBuilder sb = new StringBuilder();
-        sb.AppendLine("Index,Name,High,Center,EUp,EDown,WUp,WDown");
+        sb.AppendLine("Index,Name,High,CenterX,CenterZ,EUpX,EUpZ,WUpX,WUpZ");
 
         // bpData 출력 반복문
         for (int i = 0; i < bpData.Count; i++)
         {
-            sb.AppendLine($"{i},{bpData[i].height},{bpData[i].buildName},{bpData[i].corners[0].x},{bpData[i].corners[1].x}," +
-                $"{bpData[i].corners[2].x},{bpData[i].corners[3].x},{bpData[i].corners[4].x}");
+            sb.AppendLine($"{i},{bpData[i].height},{bpData[i].buildName}," +
+                $"{bpData[i].corners[0].x},{bpData[i].corners[0].z}," +
+                $"{bpData[i].corners[1].x},{bpData[i].corners[1].z}," +
+                $"{bpData[i].corners[2].x},{bpData[i].corners[2].z}");
         }
-        File.WriteAllText(Application.dataPath + "/03_Scripts/Pathfinding/BuildData.csv", sb.ToString());
 
-        isLoged = true;
+        File.WriteAllText(Application.dataPath + "/03_Scripts/Pathfinding/BuildData.csv", sb.ToString());
     }
 
 
     public void FindWalkableNode(Node node)      // 이동 가능 노드 탐색
     {
-        // node.nodePos이 bpData 범위 내 포함되는지 확인하고,
+        // node.nodePos이 bpData.corners[1]~bpData.corners[2] 범위 내 포함되는지 확인하고,
         // 포함되어 있다면 해당 node.walkable = flase 한다.
+
+        if (bpData == null || bpData.Count == 0) return;
+
+        for (int i = 0; i < bpData.Count; i++)
+        {
+            float minX = bpData[i].corners[1].x;
+            float maxX = bpData[i].corners[2].x;
+            float minZ = bpData[i].corners[1].z;
+            float maxZ = bpData[i].corners[2].z;
+
+            if (node.nodePos.x >= minX && node.nodePos.x <= maxX &&
+            node.nodePos.z >= minZ && node.nodePos.z <= maxZ)
+            {
+                node.walkable = false; // 건물 영역 안이므로 이동 불가
+                return; // 찾았으면 더 돌 필요 없음
+            }
+        }
+
+        // 겹치는 건물이 없으면 이동 가능 유지
+        node.walkable = true;
     }
 }
